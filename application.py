6ec764c36b443aa17afad25123012ad4cb5bdf33
Miguel Angel Angulo <maangulo12@gmail.com>
2015-05-30 18:54:33 -0500

    Change host

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ from app import app
 
 if __name__ == '__main__':
     app.run(
-        host=os.environ.get('SERVER_HOST', 'localhost'),
+        host=os.environ.get('SERVER_HOST', '0.0.0.0'),
         port=int(os.environ.get('SERVER_PORT', '5555')),
         debug=True
     )
