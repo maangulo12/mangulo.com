914963fe9656f0348467846ecf24740faa55fac3
Miguel Angulo <maangulo12@gmail.com>
2015-08-27 16:55:24 -0500

    Remove shell scripts

--- vagrant/heroku.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Installing Heroku toolbelt
-
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
-echo "Successfully installed Heroku toolbelt on this virtual machine."
